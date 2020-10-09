8f50dc84365d978b29e9141a073f542cbd6fc681
Andrew Shapton <alshapton@gmail.com>
2020-10-09 17:44:14 +0100

    Added export functionality

---
+++ kb/api/export.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+# kb v0.1.4
+# A knowledge base organizer
+# Copyright © 2020, gnc.
+# See /LICENSE for licensing information.
+
+"""
+kb export command module
+
+:Copyright: © 2020, gnc.
+:License: GPLv3 (see /LICENSE).
+"""
+
+import time
+import tarfile
+from pathlib import Path
+from typing import Dict
+from kb.actions.export import export as actionExport
+
+
+
+def export(args: Dict[str, str], config: Dict[str, str]):
+    """
+    Export the entire kb knowledge base.
+
+    Arguments:
+    args:           - a dictionary containing the following fields:
+                      file -> a string representing the wished output
+                        filename
+    config:         - a configuration dictionary containing at least
+                      the following keys:
+                      PATH_KB           - the main path of KB
+    """
+    fname=actionExport(args, config=config)
+
+    return(fname)
+    
