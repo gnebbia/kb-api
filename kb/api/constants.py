bfe95544f1653a47a91867ef244dc4a488c2b916
alshapton <alshapton@gmail.com>
2020-12-31 00:59:40 +0000

    recovered files

---
+++ kb/api/constants.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+# kb-api v0.1.0
+# A REST API for kb - the minimalist knowledge base organizer
+# Copyright © 2020, alshapton.
+# See /LICENSE for licensing information.
+
+"""
+kb-api add api module
+
+:Copyright: © 2020, alshapton.
+:License: GPLv3 (see /LICENSE).
+"""
+
+__all__ = ()
+
+
+# Dictionary of MIME types:
+MIME_TYPE = dict(json="application/json", utf8="text/plain;charset=UTF-8")
+
+# API Version
+API_VERSION = "0.1.0"
+
